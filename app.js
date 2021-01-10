1db92b2cc7c306d69f95df56a79f1729be68c6e0
Camilo Hernandez <CamiloHernandezR@noreply.com>
2021-01-09 23:17:42 -0600

    Mostrando ganadores  con javascript

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
-const puntosUsuario = 0;
-const puntosComputadora = 0;
+let puntosUsuario = 0;
+let puntosComputadora = 0;
 const puntosUsuario_span = document.getElementById("puntos-usuario");
 const puntosComputadora_span = document.getElementById("puntos-computadora");
 const marcador_div = document.querySelector(".marcador");
-const resultado_div = document.querySelector(".resultado");
+const resultado_div = document.querySelector(".resultado > h2");
 const pieda_div = document.getElementById("r");
 const tijeras_div= document.getElementById("s");
 const papel_div = document.getElementById("p");
 
+
+
 function computadoraEleccion(){
   let opciones = ['r','s','p'];
   return opciones[Math.floor(Math.random()*3)];
+}
+function getPalabra(letra){
+  return(letra === 's') ?  "tijeras" :  (letra === 'p') ? "papel" : "piedra" ;
+}
+function victoria(usuarioEleccion, eleccionComputadora){
+  puntosUsuario++;
+  puntosUsuario_span.innerHTML=puntosUsuario;
+   usuarioPalabra= "usuario".fontsize(3).sup();
+   computadoraPalabra= "comp".fontsize(3).sup();
+  resultado_div.innerHTML= `${getPalabra(usuarioEleccion)}${usuarioPalabra}  vence a   ${getPalabra(eleccionComputadora)}${computadoraPalabra} . Tú ganas."`
+}
+function derrota(usuarioEleccion, eleccionComputadora){
+  puntosComputadora++;
+  puntosComputadora_span.innerHTML=puntosUsuario;
+  resultado_div.innerHTML= `${getPalabra(usuarioEleccion)}${usuarioPalabra}  Es derrotado por   ${getPalabra(eleccionComputadora)}${computadoraPalabra} . Tú pierdes."`
+}
+function empate(usuarioEleccion, eleccionComputadora){
+  resultado_div.innerHTML= `${getPalabra(usuarioEleccion)}${usuarioPalabra}  Es igual a    ${getPalabra(eleccionComputadora)}${computadoraPalabra} . Es un empate."`
+
 }
 function game(usuarioEleccion){
   let computadora = computadoraEleccion();
@@ -18,15 +39,15 @@ function game(usuarioEleccion){
     case "pr":
     case "rs":
     case "sp":
-        console.log("El usuario gana");
+        victoria(usuarioEleccion, computadora);
       break;
       case "rp":
       case "sr":
       case "ps":
-          console.log("El usuario pierde");
+          derrota(usuarioEleccion, computadora);
         break;
       default:
-        console.log("Empate");
+          empate(usuarioEleccion, computadora);
         break;
   }
 }
